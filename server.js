0221eac31cbb3c287b7d59bf90e20368bb02d610
Wenjie <wenjiewuchem@gmail.com>
2015-08-14 16:34:49 -0700

    add server

---
+++ server.js
@@ -0,0 +1,22 @@
+// Load the express module (Where do you think this comes from?)
+var express = require("express");
+// invoke var express and store the resulting application in var app
+var app = express();
+// lets handle the base route "/" and respond with "Hello Express"
+app.use(express.static(__dirname + "/static"));
+
+// app.get('/', function(request, response) {
+//   response.render("index");
+// })
+// notice that the function is app.get(...) why do you think the function is called get?
+
+// This sets the location where express will look for the ejs views
+// app.set('views', __dirname + '/views');
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+// app.set('view engine', 'ejs');
+
+// Tell the express app to listen on port 8000
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+})
+// this line will almost always be at the end of your server.js file (we only tell the server to listen after we have set up all of our rules)
