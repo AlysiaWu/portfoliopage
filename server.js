efda1965a6e77efa946808a0fb81db11fa0a6dc2
Wenjie <wenjiewuchem@gmail.com>
2015-08-26 16:25:49 -0700

    added views

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ var app = express();
 // lets handle the base route "/" and respond with "Hello Express"
 app.use(express.static(__dirname + "/static"));
 
-// app.get('/', function(request, response) {
-//   response.render("index");
-// })
+app.get('/Xgo', function(request, response) {
+  response.render("Xgo");
+})
+
+app.get('/TaskManager', function(request, response) {
+  response.render("TaskManager");
+})
+app.get('/Xgo', function(request, response) {
+  response.render("Xgo");
+})
+app.get('/Groupster', function(request, response) {
+  response.render("Groupster");
+})
+
 // notice that the function is app.get(...) why do you think the function is called get?
 
 // This sets the location where express will look for the ejs views
-// app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views');
 // Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // Tell the express app to listen on port 8000
 app.listen(8000, function() {
