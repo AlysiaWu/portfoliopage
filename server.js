a09f7cfe3bf4bb92b884fedc7bf68b3bfec47df7
Wenjie <wenjiewuchem@gmail.com>
2015-08-26 17:32:48 -0700

    route to static

--- server.js
+++ server.js
@@ -5,19 +5,7 @@ var app = express();
 // lets handle the base route "/" and respond with "Hello Express"
 app.use(express.static(__dirname + "/static"));
 
-app.get('/Xgo', function(request, response) {
-  response.render("Xgo");
-})
 
-app.get('/TaskManager', function(request, response) {
-  response.render("TaskManager");
-})
-app.get('/Xgo', function(request, response) {
-  response.render("Xgo");
-})
-app.get('/Groupster', function(request, response) {
-  response.render("Groupster");
-})
 
 // notice that the function is app.get(...) why do you think the function is called get?
 
@@ -25,7 +13,15 @@ app.get('/Groupster', function(request, response) {
 app.set('views', __dirname + '/views');
 // Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
 app.set('view engine', 'ejs');
-
+// app.get('/TaskManager', function(request, response) {
+//   response.render("TaskManager");
+// })
+// app.get('/Xgo', function(request, response) {
+//   response.render("Xgo");
+// })
+// app.get('/Groupster', function(request, response) {
+//   response.render("Groupster");
+// })
 // Tell the express app to listen on port 8000
 app.listen(8000, function() {
   console.log("listening on port 8000");
